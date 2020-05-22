57ffff80470981b918de45a4bfda07c2eeceecee
Ilya <ilya2000269ilya5@gmail.com>
2020-05-22 09:16:42 -0700

    Add files via upload

---
+++ client.c
@@ -0,0 +1,30 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define PORT 4455
+
+int main() {
+    int clientSocket;
+    struct sockaddr_in serverAddr;
+    char buffer[1024];
+
+    clientSocket = socket(PF_INET, SOCK_STREAM, 0);
+    printf("Created \n");
+    memset(&serverAddr, '\0', sizeof(serverAddr));
+    serverAddr.sin_family=AF_INET;
+    serverAddr.sin_port=htons(PORT);
+    serverAddr.sin_addr.s_addr=inet_addr("127.0.0.1");
+
+    connect(clientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
+    printf("Connected \n");
+
+    recv(clientSocket, buffer, 1024, 0);
+    printf("Received: %s\n", buffer);
+
+    return 0;
+}
---
+++ server.c
@@ -0,0 +1,39 @@
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <arpa/inet.h>
+
+#define PORT 4455
+
+int main() {
+    int sockfd;
+    struct sockaddr_in serverAddr;
+
+    int newSocket;
+    struct sockaddr_in new_Addr;
+
+    socklen_t addr_size;
+    char buffer[1024];
+
+    sockfd=socket(PF_INET, SOCK_STREAM, 0);
+    memset(&serverAddr, '\0', sizeof(serverAddr));
+
+    serverAddr.sin_family=AF_INET;
+    serverAddr.sin_port=htons(PORT);
+    serverAddr.sin_addr.s_addr=inet_addr("127.0.0.1");
+
+    bind(sockfd, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
+
+    listen(sockfd, 5);
+    addr_size = sizeof(new_Addr);
+
+    newSocket=accept(sockfd, (struct sockaddr*)&new_Addr, &addr_size);
+
+    strcpy(buffer, "Hello");
+    send(newSocket, buffer, strlen(buffer), 0);
+
+    return 0;
+} 
